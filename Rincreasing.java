7834ddb59618b4e4aeef12c008ba29c47f6b9bd2
Shreya N <nshreya094@gmail.com>
2024-01-31 18:46:04 +0530

    Increasing order using Recursion

--- Rincreasing.java
+++ Rincreasing.java
@@ -1,9 +1,16 @@
 public class Rincreasing {
   public static void Incre(int n){
-    i
+    if(n==1){
+      System.out.println(n);
+      return;
+    }
+    Incre(n-1);
+    System.out.println(n);
 
   }
   public static void main(String[] args) {
+    int n =5;
+    Incre(n);
     
   }
   
